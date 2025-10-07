5ab911bcbeb6d63f4bc8f7ae5396f17a177b3c7e
Zorintks <zorotks@gmail.com>
2025-10-07 18:42:41 -0400

    Remover todos os arquivos do repositório

--- src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p366322384280412160',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
